1b1f413282855c289259864d76cd4edd951fba41
elvonzion <elvonzion@gmail.com>
2013-08-29 12:06:10 +0800

    sort del_same_file code

--- my-utils/del_same_files.sh
+++ my-utils/del_same_files.sh
@@ -3,7 +3,7 @@
 #cmd with  param atleast is 1 and is a valid path.
 if  [ $# -ge 2 ] && [ -d $1 ] && [ -d $2 ]
 then
-src_files=`echo $2 | tr -d "/"`
+src_dir=`echo $2 | tr -d "/"`
 dest_dir=`echo $1 | tr -d "/"`
 else
 echo "usage: $0 valid dir which existed"
@@ -12,8 +12,9 @@ exit $?
 fi
 
 #diff the dirs
-diff -r -s -q ${src_files} ${dest_dir} | grep "identical" | sed -n 's/^Files \(.*\) and \(.*\) are identical/\1 \2/p' |\
+diff -r -s -q ${src_dir} ${dest_dir} |\
+grep "identical" | sed -n 's/^Files \(.*\) and \(.*\) are identical/\1 \2/p' |\
 xargs -t rm -rf 
 
 #remove the dirs
-find ${src_files}  ${dest_dir} -type d -empty | xargs -exec rmdir -p
+find ${src_dir}  ${dest_dir} -type d -empty | xargs -exec rmdir -p
